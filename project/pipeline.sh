10d6a15417d11d832c5ebcbb075c4e458f4acde3
EnigmaticAbyss <arashmousavi193@gmail.com>
2024-05-23 09:15:59 +0200

    add

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+
+
+# Export kaggle.json - authentication
+KAGGLE_JSON_PATH="./project/kaggle.json"
+KAGGLE_CONFIG_DIR=$(dirname "$KAGGLE_JSON_PATH")
+export KAGGLE_CONFIG_DIR
+
 python3 /project/pipeline.py
-
-
-# IMPORTANT:
-## Because the data pipeline is going to connect to kaggle, it is necessary that a kaggle API token is available on the
-## local device under '~/.kaggle/kaggle.json' which extracts the data.
-## The credintials must remain secret for the security messures 
-
