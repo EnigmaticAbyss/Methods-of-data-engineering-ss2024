fd408deebb13a0d63c988d16d49330ae33b99631
EnigmaticAbyss <arashmousavi193@gmail.com>
2024-06-05 23:42:37 +0200

    add

--- project/reqIns.sh
+++ project/reqIns.sh
@@ -2,10 +2,12 @@
 
 # Install required packages
 # pandas==1.5.3
-# SQLAlchemy==1.4.46
+# SQLAlchemy==2.0.25
 # kaggle==1.5.16
+# Scikit learn==1.4.2
 
 pip install --upgrade pip
 pip install pandas
 pip install SQLAlchemy
 pip install kaggle
+pip install Scikit-learn 
