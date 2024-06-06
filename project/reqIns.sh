d1eefaf37b02a56d400096b3bbd56f73d14986aa
EnigmaticAbyss <arashmousavi193@gmail.com>
2024-06-06 11:48:44 +0200

    project complete

--- project/reqIns.sh
+++ project/reqIns.sh
@@ -5,9 +5,11 @@
 # SQLAlchemy==2.0.25
 # kaggle==1.5.16
 # Scikit learn==1.4.2
+# statsmodels== 0.14.02
 
 pip install --upgrade pip
 pip install pandas
 pip install SQLAlchemy
 pip install kaggle
 pip install Scikit-learn 
+pip install statsmodels
