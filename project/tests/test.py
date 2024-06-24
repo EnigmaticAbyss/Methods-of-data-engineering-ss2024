c7b8dde2b80e80c2526dac74b1e777ce9a111e6a
EnigmaticAbyss <arashmousavi193@gmail.com>
2024-06-24 22:03:02 +0200

    test

--- project/tests/test.py
+++ project/tests/test.py
@@ -1,8 +1,14 @@
 
 import os
+import sys
 # Get the current directory of child_script.py
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Construct the path to the parent_script.py
 parent_script_path = os.path.join(current_dir, '..', 'parent_script.py')
 print(parent_script_path)
+
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+sys.path.append(parent_dir)
+print(parent_dir)
--- project/tests/test_pipeline.py
+++ project/tests/test_pipeline.py
@@ -1,24 +1,15 @@
 import os
+import sys
 import subprocess
 import pytest
 
 @pytest.fixture(scope="module")
 def run_pipeline():
     # Path to the pipeline script
- 
-    # Step 1: Get the directory of the current script
-    # current_script_dir = os.path.dirname(os.path.abspath(__file__))
     
-    # # Step 2: Get the parent directory
-    # parent_dir = os.path.abspath(os.path.join(current_script_dir, os.pardir))
-    
-    # # Step 3: Construct the path to the target file in the parent directory
-    # target_file_path = os.path.join(parent_dir,'pipeline.py')
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Construct the path to the parent_script.py
-    parent_script_path = os.path.join(current_dir, '..', 'pipeline.py')
-    # Execute the pipeline
+    parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    sys.path.append(parent_dir)
+    parent_script_path = os.path.join(parent_dir, 'parent_script.py')
     subprocess.run(['python', parent_script_path], check=True)
     
     yield
